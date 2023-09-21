2e2b142704ad152b7c2198e91c034027f1a5d57b
Sai krishna kotnala <saikrishnachinnu8@gmail.com>
2023-09-21 16:13:14 +0530

    Update crudapi.py

--- crudapi.py
+++ crudapi.py
@@ -1,55 +1,68 @@
-from flask import Flask, request, jsonify
+class LeaveRequest:
+    def __init__(self, employee_name, start_date, end_date):
+        self.employee_name = employee_name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.approved = False
 
-app = Flask(__name__)
+    def approve(self):
+        self.approved = True
 
-# Sample data (in-memory storage)
-items = []
+    def __str__(self):
+        return f"Employee: {self.employee_name}, Start Date: {self.start_date}, End Date: {self.end_date}, Approved: {self.approved}"
 
-# Create an item
-@app.route('/items', methods=['POST'])
-def create_item():
-    data = request.get_json()
-    if 'name' in data:
-        new_item = {
-            'id': len(items) + 1,
-            'name': data['name']
-        }
-        items.append(new_item)
-        return jsonify(new_item), 201
-    else:
-        return jsonify({'error': 'Name is required'}), 400
+class LeaveApplicationSystem:
+    def __init__(self):
+        self.leave_requests = []
 
-# Read all items
-@app.route('/items', methods=['GET'])
-def get_all_items():
-    return jsonify({'items': items})
+    def request_leave(self):
+        print("Leave Application")
+        employee_name = input("Enter your name: ")
+        start_date = input("Enter start date (yyyy-mm-dd): ")
+        end_date = input("Enter end date (yyyy-mm-dd): ")
 
-# Read a specific item
-@app.route('/items/<int:item_id>', methods=['GET'])
-def get_item(item_id):
-    item = next((item for item in items if item['id'] == item_id), None)
-    if item:
-        return jsonify(item)
-    else:
-        return jsonify({'error': 'Item not found'}), 404
+        request = LeaveRequest(employee_name, start_date, end_date)
+        self.leave_requests.append(request)
+        print("Leave request submitted.")
 
-# Update an item
-@app.route('/items/<int:item_id>', methods=['PUT'])
-def update_item(item_id):
-    data = request.get_json()
-    item = next((item for item in items if item['id'] == item_id), None)
-    if item:
-        item['name'] = data.get('name', item['name'])
-        return jsonify(item)
-    else:
-        return jsonify({'error': 'Item not found'}), 404
+    def process_leave_requests(self):
+        print("Leave Requests")
+        for i, request in enumerate(self.leave_requests, start=1):
+            print(f"{i}. {request}")
 
-# Delete an item
-@app.route('/items/<int:item_id>', methods=['DELETE'])
-def delete_item(item_id):
-    global items
-    items = [item for item in items if item['id'] != item_id]
-    return jsonify({'message': 'Item deleted'})
+        choice = input("Enter the number of the request to approve (0 to exit): ")
+        if choice == '0':
+            return
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        try:
+            choice = int(choice)
+            if 1 <= choice <= len(self.leave_requests):
+                request = self.leave_requests[choice - 1]
+                request.approve()
+                print("Leave request approved.")
+            else:
+                print("Invalid choice.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    def run(self):
+        while True:
+            print("\nLeave Application System")
+            print("1. Request Leave")
+            print("2. Process Leave Requests")
+            print("3. Exit")
+            choice = input("Enter your choice: ")
+
+            if choice == '1':
+                self.request_leave()
+            elif choice == '2':
+                self.process_leave_requests()
+            elif choice == '3':
+                print("Exiting the application.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    leave_system = LeaveApplicationSystem()
+    leave_system.run()
