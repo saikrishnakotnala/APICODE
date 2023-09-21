71430b53f9ea7a4a843f933386d27eeecd69b9dc
Sai krishna kotnala <saikrishnachinnu8@gmail.com>
2023-09-21 16:11:18 +0530

    Update API.java

--- API.java
+++ API.java
@@ -1,65 +1,120 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class APIClientApp extends Application {
+class LeaveRequest {
+    private String employeeName;
+    private String startDate;
+    private String endDate;
+    private boolean approved;
 
-    public static void main(String[] args) {
-        launch(args);
+    public LeaveRequest(String employeeName, String startDate, String endDate) {
+        this.employeeName = employeeName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.approved = false;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void approve() {
+        approved = true;
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("API Client Example");
+    public String toString() {
+        return "LeaveRequest{" +
+                "employeeName='" + employeeName + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", approved=" + approved +
+                '}';
+    }
+}
 
-        // Create a label to display the API response
-        Label responseLabel = new Label("API Response Will Appear Here");
-   
-        // Create a VBox layout to hold the label
-        VBox vbox = new VBox(responseLabel);
-      
-    
-        // Create a scene
-        Scene scene = new Scene(vbox, 300, 200);
+class LeaveApplicationSystem {
+    private List<LeaveRequest> leaveRequests = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
 
-        // Set the scene
-        primaryStage.setScene(scene);
+    public void requestLeave() {
+        System.out.println("Leave Application");
+        System.out.print("Enter your name: ");
+        String employeeName = scanner.nextLine();
+        System.out.print("Enter start date (yyyy-mm-dd): ");
+        String startDate = scanner.nextLine();
+        System.out.print("Enter end date (yyyy-mm-dd): ");
+        String endDate = scanner.nextLine();
 
-        // Make an API request and set the response as the label text
-        try {
-            String apiResponse = makeAPIRequest("https://jsonplaceholder.typicode.com/posts/1");
-            responseLabel.setText(apiResponse);
-        } catch (Exception e) {
-            e.printStackTrace();
-            responseLabel.setText("Error: Unable to fetch data from the API");
-        }
-
-        // Show the stage
-        primaryStage.show();
+        LeaveRequest request = new LeaveRequest(employeeName, startDate, endDate);
+        leaveRequests.add(request);
+        System.out.println("Leave request submitted.");
     }
 
-    private String makeAPIRequest(String apiUrl) throws Exception {
-        URL url = new URL(apiUrl);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.connect();
-
-        int responseCode = conn.getResponseCode();
-        if (responseCode == 200) {
-            Scanner scanner = new Scanner(url.openStream());
-            StringBuilder response = new StringBuilder();
-            while (scanner.hasNext()) {
-                response.append(scanner.nextLine());
-            }
-            scanner.close();
-            return response.toString();
-        } else {
-            throw new Exception("API request failed with response code: " + responseCode);
+    public void processLeaveRequests() {
+        System.out.println("Leave Requests");
+        for (int i = 0; i < leaveRequests.size(); i++) {
+            LeaveRequest request = leaveRequests.get(i);
+            System.out.println((i + 1) + ". " + request);
         }
+
+        System.out.print("Enter the number of the request to approve (0 to exit): ");
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        if (choice >= 1 && choice <= leaveRequests.size()) {
+            LeaveRequest request = leaveRequests.get(choice - 1);
+            request.approve();
+            System.out.println("Leave request approved.");
+        } else if (choice != 0) {
+            System.out.println("Invalid choice.");
+        }
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println("\nLeave Application System");
+            System.out.println("1. Request Leave");
+            System.out.println("2. Process Leave Requests");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    requestLeave();
+                    break;
+                case 2:
+                    processLeaveRequests();
+                    break;
+                case 3:
+                    System.out.println("Exiting the application.");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LeaveApplicationSystem leaveSystem = new LeaveApplicationSystem();
+        leaveSystem.run();
     }
 }
